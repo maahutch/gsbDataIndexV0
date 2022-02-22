1d3fc34c3f9bd5a2a64b9fcae2ba8aaa0172a3d5
Matthew Hutchinson <maahutch@stanford.edu>
2022-02-22 14:16:58 -0800

    Adding stoage endpoint

--- data_api/app.py
+++ data_api/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 import os.path
-from roamPy import Roam
+from roamPy.roamClass import Roam
 import redivis
 from neo4jClass import Neo
 
 app = Flask(__name__)
 
-roamToken    = os.environ.get('Roam_API_Key')
-#redivisToken = os.environ.get('Redivis_API_Key')
-dbPwd        = os.environ.get('Neo4j_password')
+app.config['roamToken']         = os.environ.get('Roam_API_Key')
+app.config['REDIVIS_API_TOKEN'] = os.environ.get('REDIVIS_API_TOKEN')
+app.config['dbPwd']             = os.environ.get('Neo4j_password')
 
 @app.route('/')
 def home():
@@ -19,10 +19,25 @@ def home():
 @app.route('/storage/<dataset>')
 def getStorage(dataset):
 
+    uri = "bolt://localhost:7687"
+    user = "neo4j"
+    password = os.environ.get('Neo4j_password')
+    connNeo = Neo(uri = uri, user = user, password=password)
 
+    dataset_Neo = connNeo.getStorage(dataset = dataset)
 
+    if dataset_Neo[0][0] == 'Redivis':
 
-    dataset = redivis.organization("StanfordGSBLibrary").dataset(dataset)
+            dataset_Red = redivis.organization("StanfordGSBLibrary").dataset(dataset)
+            
+            dataset_Red.get()
+
+            return(jsonify({'Redivis': dataset_Red.properties, 'Neo4j': dataset_Neo}))
+    else:
+        return(jsonify(dataset_Neo))
+
+    #
+
 
 #@app.route('/license/<dataset>')
 
--- data_api/neo4jClass.py
+++ data_api/neo4jClass.py
@@ -5,7 +5,7 @@ import json
 class Neo: 
 
     def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.driver = GraphDatabase.driver(uri, auth=(user, password), encrypted=False)
 
     def close(self):
         self.driver.close()
@@ -22,6 +22,18 @@ class Neo:
 
     def getStorage(self, dataset):
         with self.driver.session() as session:
-            result = session.read_transaction(self,__getStorage)
-            storage = json.dumps(result, indent = 4)
+            result = session.read_transaction(self.__getStorage, dataset)
+            storage = list(result)
             return(storage)
+
+
+# if __name__ =="__main__":
+#    uri = "bolt://127.0.0.1:7687"
+#    user = "neo4j"
+#    password = 'gsb_data_index'
+
+#    neo = Neo(uri, user, password)
+
+#    print(neo.getStorage('Berkely Options'))
+
+#    neo.close()
